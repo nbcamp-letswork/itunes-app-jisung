2157a80d4e1382f265c0e62c61d449085a5f7e7c
JISUNG PARK <meowbutlerdev@gmail.com>
2025-05-13 19:32:23 +0900

    feat: 검색 결과 Remaining 섹션 구현

--- iTunesAppJisung/Presentation/Scene/Search/ResultConstant.swift
+++ iTunesAppJisung/Presentation/Scene/Search/ResultConstant.swift
@@ -19,4 +19,20 @@ enum ResultConstant {
         static let sectionHeight: CGFloat = 500
         static let sectionVerticalSpacing: CGFloat = 32
     }
+
+    enum Remaining {
+        static let imageCornerRadius: CGFloat = 16
+        static let fontSize: CGFloat = 18
+        static let labelSpacing: CGFloat = 2
+        static let imageSize = 64
+        static let stackViewLeading = 12
+        static let backgroundCornerRadius: CGFloat = 20
+        static let titleFontSize: CGFloat = 28
+        static let tableSpacing: CGFloat = 16
+        static let headerHeight: CGFloat = 60
+        static let tableCellHeight: CGFloat = 80
+        static let closeButtonSpacing = 20
+        static let closeButtonSize = 36
+        static let sectionVerticalSpacing: CGFloat = 32
+    }
 }
