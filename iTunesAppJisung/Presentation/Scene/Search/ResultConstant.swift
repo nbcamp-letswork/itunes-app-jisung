32a643b3581a8672654417148179857337bcfcb6
JISUNG PARK <meowbutlerdev@gmail.com>
2025-05-13 19:29:10 +0900

    feat: 검색 결과 Spotlight 섹션 구현

--- iTunesAppJisung/Presentation/Scene/Search/ResultConstant.swift
+++ iTunesAppJisung/Presentation/Scene/Search/ResultConstant.swift
@@ -5,4 +5,18 @@ enum ResultConstant {
         static let fontSize: CGFloat = 34
         static let spacing = 16
     }
+
+    enum Spotlight {
+        static let backgroundCornerRadius: CGFloat = 20
+        static let titleFontSize: CGFloat = 16
+        static let subTitleFontSize: CGFloat = 20
+        static let titleSpacing = 16
+        static let titleHeight = 20
+        static let subTitleTopSpacing = 8
+        static let subTitleHorizontalSpacing = 16
+        static let subTitleHeight = 25
+        static let imageTopSpacing = 8
+        static let sectionHeight: CGFloat = 500
+        static let sectionVerticalSpacing: CGFloat = 32
+    }
 }
